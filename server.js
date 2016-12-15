45616083e5d07b854fa314df9b10cdc561d348c4
ernie <ernie@epicbytes.net>
2016-12-15 13:53:29 +0100

    server.js

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 var MailDev = require('maildev');
 
 var maildev = new MailDev({
-  smtp: 443 // incoming SMTP port - default is 1025
+  web: process.env.PORT || 1080,
+  smtp: process.env.SMTP || 1025
 });
 
-maildev.listen(function(err) {
-  console.log('We can now sent emails to port 1025!');
-});
-
-// Print new emails to the console as they come in
-maildev.on('new', function(email){
-  console.log('Received new email with subject: %s', email.subject);
-});
-
-// Get all emails
-maildev.getAllEmail(function(err, emails){
-  if (err) return console.log(err);
-  console.log('There are %s emails', emails.length);
-});
+maildev.listen();
